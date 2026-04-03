00739a3796c899f34a730cde838ab314f91855a9
HepzibhaSundararaj <{hepzibha.sundar@gmail.com}>
2026-04-02 18:31:19 -0700

    Error handling

---
+++ errorHandling.js
@@ -0,0 +1,7 @@
+/*
+JavaScript has built-in error types (TypeError, RangeError, etc.) 
+and lets you create custom errors by extending the Error class.  
+Use finally for cleanup that must always run (like closing connections.)
+*/
+
+//Custom Error class
